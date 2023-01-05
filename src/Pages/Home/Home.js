d0a2ab7afd71f99e39627329ed5b98e32d863686
RobinTheRedLight <robinmitra789@gmail.com>
2023-01-05 15:29:14 +0600

    About Section Added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -10,47 +10,51 @@ const Home = () => {
 
             <div className='col-span-8 border-2 border-white rounded-lg'>
                 <div className='p-2'>
-                    <div className="hero h-96" style={{ backgroundImage: `url("1.jpg")` }}>
-                        <div className="hero-overlay bg-opacity-60"></div>
-                        <div className="hero-content text-center text-neutral-content">
-                            <div className="max-w-md">
-                                <h1 className="text-slate-200 mb-5 text-5xl font-bold">Robin's Law Desk</h1>
-                                <p className="font-mono text-slate-200 mb-5">Our goal is to provide high-quality legal services that help our clients to achieve their goals and resolve their legal issues.</p>
+                    <div className='border-2 border-white rounded-lg p-1'>
+                        <div className="hero h-96" style={{ backgroundImage: `url("1.jpg")` }}>
+                            <div className="hero-overlay bg-opacity-60"></div>
+                            <div className="hero-content text-center text-neutral-content">
+                                <div className="max-w-md">
+                                    <h1 className="text-slate-200 mb-5 text-5xl font-bold">Robin's Law Desk</h1>
+                                    <p className="font-mono text-slate-200 mb-5">Our goal is to provide high-quality legal services that help our clients to achieve their goals and resolve their legal issues.</p>
 
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='py-2'>
+                        <div className="p-10 g-base-200 border-2 border-white rounded-lg">
+                            <div className="grid grid-cols-3 gap-4 ">
+                                <div className='col-span-1 border-r-4 flex items-center'>
+                                    <h1 className="text-5xl font-bold">About Us</h1>
+                                </div>
+                                <p className="text-justify col-span-2 ">As a law firm, we offer a variety of legal services to meet the needs of our clients. Our team of attorneys is dedicated to providing high-quality legal representation and advice to individuals and businesses alike. We specialize in a range of areas, including criminal law, civil litigation, family law, and corporate law, to name a few. Our attorneys have extensive experience and are committed to helping our clients navigate the legal system and achieve their goals. Whether you need representation in court, assistance with legal documents, or simply need advice on a legal matter, we are here to help.</p>
                             </div>
                         </div>
                     </div>
 
-                    <div className="hero min-h-screen bg-base-200">
-                        <div className="hero-content text-center">
-                            <div className="max-w-md">
-                                <h1 className="text-5xl font-bold">Hello there</h1>
-                                <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                                <button className="btn btn-primary">Get Started</button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="stats bg-primary text-primary-content">
+                    <div className="stats stats-vertical lg:stats-horizontal shadow">
 
                         <div className="stat">
-                            <div className="stat-title">Account balance</div>
-                            <div className="stat-value">$89,400</div>
-                            <div className="stat-actions">
-                                <button className="btn btn-sm btn-success">Add funds</button>
-                            </div>
+                            <div className="stat-title">Downloads</div>
+                            <div className="stat-value">31K</div>
+                            <div className="stat-desc">Jan 1st - Feb 1st</div>
                         </div>
 
                         <div className="stat">
-                            <div className="stat-title">Current balance</div>
-                            <div className="stat-value">$89,400</div>
-                            <div className="stat-actions">
-                                <button className="btn btn-sm">Withdrawal</button>
-                                <button className="btn btn-sm">deposit</button>
-                            </div>
+                            <div className="stat-title">New Users</div>
+                            <div className="stat-value">4,200</div>
+                            <div className="stat-desc">↗︎ 400 (22%)</div>
+                        </div>
+
+                        <div className="stat">
+                            <div className="stat-title">New Registers</div>
+                            <div className="stat-value">1,200</div>
+                            <div className="stat-desc">↘︎ 90 (14%)</div>
                         </div>
 
                     </div>
+
                 </div>
             </div>
 

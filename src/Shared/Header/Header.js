81b6e56c0d7c6b8758ddf6119e61aefa05d60923
RobinTheRedLight <robinmitra789@gmail.com>
2023-01-07 00:39:16 +0600

    Navbar Added

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -18,7 +18,7 @@ const Header = () => {
 
     return (
         <div>
-            <div className="navbar bg-accent">
+            <div className="navbar bg-base-100 border-2 border-white rounded-lg">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -27,6 +27,7 @@ const Header = () => {
                         <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
 
                             <div>
+                                <Link className='pt-3' to='/services'>Services</Link>
                                 <div className='pt-1'>
                                     <Link>
                                         {user?.photoURL ?
@@ -44,8 +45,10 @@ const Header = () => {
                                     {
                                         user?.uid ?
                                             <>
-                                                <span>{user?.displayName}</span>
                                                 <Link onClick={handleLogOut} className=' pt-3 block'>Logout</Link>
+                                                <Link className='pt-3' to='/reviews'>My reviews</Link>
+                                                <Link className='pt-3' to='/addservice'>Add service</Link>
+
                                             </>
                                             :
                                             <>
@@ -58,12 +61,10 @@ const Header = () => {
                                 </div>
 
                             </div>
-                            <Link className='pt-3' to='/courses'>Courses</Link>
-                            <Link className='pt-3'>FAQ</Link>
                             <Link className='pt-3' to='/Blog'>Blog</Link>
                         </ul>
                     </div>
-                    <Link className=' font-mono text-white btn btn-ghost normal-case sm:text-2xl' to='/'>Learn With Robin</Link>
+                    <Link className=' font-mono text-white btn btn-ghost normal-case sm:text-2xl' to='/'>Robin's Law Desk</Link>
                     <div className="w-10">
                         <img src="logo.png" />
                     </div>
@@ -71,50 +72,39 @@ const Header = () => {
 
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal p-0 text-white">
-                        <div className='flex'>
-                            <div className='pt-1'>
-                                {
-                                    user?.uid ?
-                                        <>
-                                            <span>Welcome, {user?.displayName}</span>
-                                            <Link onClick={handleLogOut} className=' text-xl pt-1 px-3'>Logout</Link>
-                                        </>
-                                        :
-                                        <>
-                                            <Link className='text-xl pt-1 px-3' to='/login'>Login</Link>
-                                            <Link className='text-xl pt-1 px-3' to='/register'>Register</Link>
-                                        </>
-                                }
 
-
-                            </div>
-                            <div className='pt-1'>
-                                <Link>
-                                    {user?.photoURL ?
-                                        <img title={user.displayName} className='h-8 rounded-full' src={user?.photoURL} alt="" />
-                                        :
-                                        <>
-                                            <h1 onClick={notify} className='text-xl pt-1 px-3'><FaUser></FaUser></h1>
-                                            <Toaster />
-                                        </>
-                                    }
-                                </Link>
-                            </div>
-                        </div>
-                        <Link className=' text-xl pt-1 px-3' to='/courses'>Courses</Link>
-                        <Link className=' text-xl pt-1 px-3'>FAQ</Link>
                         <Link className=' text-xl pt-1 px-3' to='/Blog'>Blog</Link>
+                        <Link className=' text-xl pt-1 px-3' to='/services'>Services</Link>
+                        <div className='pt-1'>
+                            <Link>
+                                {user?.photoURL ?
+                                    <img title={user.displayName} className='h-8 rounded-full' src={user?.photoURL} alt="" />
+                                    :
+                                    <>
+                                        <h1 onClick={notify} className='text-xl pt-1 px-3'><FaUser></FaUser></h1>
+                                        <Toaster />
+                                    </>
+                                }
+                            </Link>
+                        </div>
+                        <div className='pt-1'>
+                            {
+                                user?.uid ?
+                                    <>
+                                        <Link onClick={handleLogOut} className=' text-xl pt-1 px-3'>Logout</Link>
+                                        <Link className=' text-xl pt-1 px-3' to='/reviews'>My reviews</Link>
+                                        <Link className=' text-xl pt-1 px-3' to='/addservice'>Add service</Link>
+                                    </>
+                                    :
+                                    <>
+                                        <Link className='text-xl pt-1 px-3' to='/login'>Login</Link>
+                                        <Link className='text-xl pt-1 px-3' to='/register'>Register</Link>
+                                    </>
+                            }
+                        </div>
                     </ul>
                 </div>
                 <div className="navbar-end">
-
-                    <div>
-                        <label className="inline-flex relative items-center cursor-pointer">
-                            <input type="checkbox" value="" className="sr-only peer" />
-                            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-
-                        </label>
-                    </div>
                 </div>
             </div>
         </div >
